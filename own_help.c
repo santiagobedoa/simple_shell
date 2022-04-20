a04a6a77d054cd8df5b8efaaa66c55cdfb0182f4
Santiago Bedoya <santiagobedoa@gmail.com>
2022-04-20 17:56:57 -0500

    minor improvements to help.c

--- own_help.c
+++ own_help.c
@@ -18,13 +18,14 @@ int own_help(char **args)
 
 	if (args)
 	{
-		printf("simple shell");
-		printf("Type a command and its arguments and hit enter");
+		printf("\n---help simple_shell---\n");
+		printf("Type a command and its arguments, then hit enter\n");
+		printf("Built-in commands:\n");
 		for (; i < num_builtins(); i++)
 		{
-			printf("  ->%s\n", builtin_func_list[i]);
+			printf("  -> %s\n", builtin_func_list[i]);
 		}
 	}
-	printf("Use the man command for information on other programs.\n");
+	printf("Use the man command for information on other programs.\n\n");
 	return (1);
 }
